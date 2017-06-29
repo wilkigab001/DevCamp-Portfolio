bea5fbcd2f1b16f92d703af54b09e079358c1c1d
wilkigab001 <wilkigab001@gmail.com>
2017-06-29 17:43:09 +0000

    Added guest user for current user

--- DevcampProgram/app/controllers/portfolios_controller.rb
+++ DevcampProgram/app/controllers/portfolios_controller.rb
@@ -14,7 +14,7 @@ class PortfoliosController < ApplicationController
 
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(potfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -33,7 +33,7 @@ class PortfoliosController < ApplicationController
   def update
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
@@ -55,4 +55,14 @@ class PortfoliosController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  private
+  
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                      )
+  end 
 end
