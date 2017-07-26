f5bfcc290e8f67b6fa612ccb769b6832ff8f0cfc
wilkigab001 <wilkigab001@gmail.com>
2017-07-26 14:11:25 +0000

    updated file upload forms and refactored placeholder functionality into view

--- DevcampProgram/app/models/portfolio.rb
+++ DevcampProgram/app/models/portfolio.rb
@@ -3,6 +3,9 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies, reject_if: lambda {|attrs| attrs['name'].blank?}
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
+  
+  #mount_uploader :main_image, PortfolioUploader
+  #mount_uploader :thumb_image, PortfolioUploader
 
   def self.angular
     where(subtitle: 'Angular')
